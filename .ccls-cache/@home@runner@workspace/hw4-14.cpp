9675cb7face3258a3c732ba27c6c828a7d9fce21
leighaja <lkelsosc@gmail.com>
2025-03-12 00:23:03 +0000

    submission of hw 4-14

--- .ccls-cache/@home@runner@workspace/hw4-14.cpp
+++ .ccls-cache/@home@runner@workspace/hw4-14.cpp
@@ -5,7 +5,48 @@
 using namespace std;
 
 int main() {
-  
+  double smallPizzas = 0.0;
+  double mediumPizzas =0.0;
+  double largePizzas = 0.0;
+  double familyPizzas = 0.0;
 
+  double totalPizzas = 0.0;
+
+  double smallPizzaPercent = 0.0;
+
+  double mediumPizzaPercent = 0.0;
+
+  double largePizzaPercent = 0.0;
+
+  double familyPizzaPercent = 0.0;
+
+  cout << "Enter number of small pizzas sold: ";
+  cin >> smallPizzas;
+  cout << "Enter number of medium pizzas sold: ";
+  cin >> mediumPizzas;
+  cout << "Enter number of large pizzas sold: ";
+  cin >> largePizzas;
+  cout << "Enter number of family pizzas sold: ";
+  cin >> familyPizzas;
+totalPizzas = smallPizzas + mediumPizzas + largePizzas + familyPizzas;
+
+  smallPizzaPercent = (smallPizzas / totalPizzas) * 100;
+
+  mediumPizzaPercent = (mediumPizzas / totalPizzas) * 100;
+  
+  largePizzaPercent = (largePizzas / totalPizzas) * 100;
+
+  familyPizzaPercent = (familyPizzas / totalPizzas) * 100;
+
+  cout << "Total pizzas sold: " << totalPizzas;
+
+  cout << "Small pizza percent: " << smallPizzaPercent;
+
+  cout << "Medium pizza percent: " << mediumPizzaPercent;
+
+  cout << "Large pizza percent: " << largePizzaPercent;
+
+  cout << "Family pizza percent: " << familyPizzaPercent << endl;
+  
   return 0;
 } // end of main function
